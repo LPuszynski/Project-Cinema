0d3f44073507bce6d29235ce59ea79571ecd6fb4
choyo <charlotte.lambert92@gmail.com>
2020-11-16 20:21:43 +0100

    conflitPART2

--- Project-Cinema/src/project/ing3/cinema/ProjectING3Cinema.java
+++ Project-Cinema/src/project/ing3/cinema/ProjectING3Cinema.java
@@ -16,7 +16,7 @@ public class ProjectING3Cinema {
      */
     public static void main(String[] args) {
         System.out.println("Double SKRTTTTTTTTTTTTTTTTTT");
-        System.out.println("blablabla test Camille");    
+        System.out.println("blablabla test conflit2");    
     }
     
 }
