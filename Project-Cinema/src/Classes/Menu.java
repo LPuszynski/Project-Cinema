c6c46c85f2aa035b98830220485bbeb460f4d3eb
choyo <charlotte.lambert92@gmail.com>
2020-11-17 21:20:12 +0100

    essai connect my sql

--- Project-Cinema/src/Classes/Menu.java
+++ Project-Cinema/src/Classes/Menu.java
@@ -5,15 +5,46 @@
  */
 package Classes;
 
+import java.sql.*;
+
 /**
  *
  * @author charl
  */
 public class Menu {
+<<<<<<< Updated upstream
 
     public static void main(String[] args) {
         Date date = new Date(10, 9, 2000);
         date.display();
+=======
+    
+    public static void main(String[] args) throws SQLException
+    {
+        //https://www.youtube.com/watch?v=54mszeznwtc
+        Connection conn = null;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jbdc:mysql://localhost:3306/test";
+            String user = "root";
+            String password = "" ;      
+            
+            /*conn = DriveManager.getConnection(url,user, password);
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY);
+            
+            ResultSet resultSet = stmt.executeQuery("Select * from movies");
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }*/
+        }
+        catch(Exception e )
+        {
+            System.out.println("pb connection database");
+        }
+>>>>>>> Stashed changes
         
     }
+    
 }
