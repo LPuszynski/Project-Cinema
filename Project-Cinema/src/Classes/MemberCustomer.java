5f12bd6691bf65430a3801f91ff9810d94dd554d
LPuszynski <61579255+LPuszynski@users.noreply.github.com>
2020-11-17 19:03:46 +0100

    same

--- Project-Cinema/src/Classes/MemberCustomer.java
+++ Project-Cinema/src/Classes/MemberCustomer.java
@@ -13,8 +13,37 @@ public class MemberCustomer extends Customer {
     private String login;
     private String password;
     private String pass; ///forfait : children, regular, senior
+    private double discount;
+
+    public MemberCustomer(String login, String password, String pass) {
+        this.login = login;
+        this.password = password;
+        this.pass = pass;
+    }
+    
+    public void setDiscount()
+    {
+        if(pass == "children")
+        {
+            discount = 0.2;
+        }
+        
+        else if (pass == "regular")
+        {
+            discount = 0.15;
+        }
+        
+        else if (pass == "senior")
+        {
+            discount = 0.1;
+        }
+    }
+    
+    
     
     @Override
-    public double getPrice(){return 0;}
+    public double getPrice(Movie movie){
+        return movie.getTicketPrice()-movie.getTicketPrice()*discount;
+    }
     
 }
