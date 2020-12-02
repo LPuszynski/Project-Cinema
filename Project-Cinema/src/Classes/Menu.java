a56557b12a9b54be2f738b5364d1d9d966d733d3
LPuszynski <61579255+LPuszynski@users.noreply.github.com>
2020-12-02 17:12:23 +0100

    ne pas prendre en compte

--- Project-Cinema/src/Classes/Menu.java
+++ Project-Cinema/src/Classes/Menu.java
@@ -3,15 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 //SOURCES : 
 //https://www.mediaforma.com/java-acces-a-la-base-de-donnees-dans-netbeans/
 //Lectures by Mme Paranjape
+
 package Classes;
 
 import java.sql.*;
 import com.mysql.jdbc.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 /**
  *
@@ -21,70 +24,71 @@ public class Menu {
 
     public static void main(String[] args) {
         /*
-         Date date = new Date(10, 9, 2000);
-         date.display();
+        Date date = new Date(10, 9, 2000);
+        date.display();
         
-         Connection conn = null;
-         String[] colNames;
-         String[][] tableData;
+        Connection conn = null;
+        String[] colNames;
+        String[][] tableData;
         
-         try{
-         //on établit une connexion avec la base de données java
+        try{
+            //on établit une connexion avec la base de données java
                     
-         Class.forName("com.mysql.jdbc.Driver");
-         String url = "jdbc:mysql://localhost:3306/projet?useSSL=false";
-         String user = "root";
-         String password = "" ;
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/projet?useSSL=false";
+            String user = "root";
+            String password = "" ;
             
-         conn = (Connection) DriverManager.getConnection(url,user, password);
+            conn = (Connection) DriverManager.getConnection(url,user, password);
            
-         //on crée un objet Statement. 
-         //Par son intermédiaire, on pourra exécuter des commandes SQL pour interroger la base de données et obtenir les résultats correspondants.
-         Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY);
+            //on crée un objet Statement. 
+            //Par son intermédiaire, on pourra exécuter des commandes SQL pour interroger la base de données et obtenir les résultats correspondants.
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY);
             
-         // on définit une requête SQL en créant un objet ResultSet à partir de l’objet Statement
-         ResultSet resultSet = stmt.executeQuery("Select * from film");
+            // on définit une requête SQL en créant un objet ResultSet à partir de l’objet Statement
+            ResultSet resultSet = stmt.executeQuery("Select * from film");
             
-         resultSet.last(); //on va au dernier élément pour savoir la taille
-         int nbRows = resultSet.getRow(); // il y a nbRows lignes dans la table
-         resultSet.first(); //on remet le pointeur en début de table
+            resultSet.last(); //on va au dernier élément pour savoir la taille
+            int nbRows = resultSet.getRow(); // il y a nbRows lignes dans la table
+            resultSet.first(); //on remet le pointeur en début de table
             
-         //on récupère les métadonnées de la table
-         //Par leur intermédiaire, on pourra obtenir le nombre de colonnes dans la table avec getColumnCount() et le nom des colonnes avec getColumnName() :
-         ResultSetMetaData resultMeta = resultSet.getMetaData();
+            //on récupère les métadonnées de la table
+            //Par leur intermédiaire, on pourra obtenir le nombre de colonnes dans la table avec getColumnCount() et le nom des colonnes avec getColumnName() :
+            ResultSetMetaData resultMeta = resultSet.getMetaData();
             
-         colNames = new String[resultMeta.getColumnCount()]; //on initialise autant de case dans le tableau qu'il y a de colonnes dans la table
+            colNames = new String[resultMeta.getColumnCount()]; //on initialise autant de case dans le tableau qu'il y a de colonnes dans la table
             
-         for(int i=0; i<resultMeta.getColumnCount(); i++)
-         {
-         //System.out.println(resultMeta.getColumnName(i+1));
-         colNames[i] = resultMeta.getColumnLabel(i+1); //ou sinon utiliser getColumnName
-         }
+            for(int i=0; i<resultMeta.getColumnCount(); i++)
+            {
+                //System.out.println(resultMeta.getColumnName(i+1));
+                colNames[i] = resultMeta.getColumnLabel(i+1); //ou sinon utiliser getColumnName
+            }
 
-         //on pour récupère tous les enregistrements de la table :
-         tableData = new String[nbRows][resultMeta.getColumnCount()]; //allouer de la place
-         for (int row=0; row<nbRows; row++)
-         {
-         for(int col=0; col<resultMeta.getColumnCount(); col++)
-         {
-         tableData[row][col] = resultSet.getString(col+1);
-         }
-         resultSet.next();
-         }
+            //on pour récupère tous les enregistrements de la table :
+            tableData = new String[nbRows][resultMeta.getColumnCount()]; //allouer de la place
+            for (int row=0; row<nbRows; row++)
+            {
+                for(int col=0; col<resultMeta.getColumnCount(); col++)
+                {
+                    tableData[row][col] = resultSet.getString(col+1);
+                }
+                resultSet.next();
+            }
             
             
-         TableFormatter table = new TableFormatter(tableData, colNames);
+            TableFormatter table = new TableFormatter(tableData, colNames);
             
-         while (resultSet.next())
-         {
-         System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
-         }
+            while (resultSet.next())
+            {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }
             
-         //ferme les objets ouverts avec la méthode close() :
-         resultSet.close();
-         stmt.close();
-         conn.close();
+             //ferme les objets ouverts avec la méthode close() :
+            resultSet.close();
+            stmt.close();
+            conn.close();
             
+<<<<<<< HEAD
          }
          catch(SQLException | ClassNotFoundException e )
          {
@@ -182,17 +186,34 @@ public class Menu {
                     + ")");*/
         } catch (SQLException ex) {
             Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, null, ex);
+=======
         }
+        catch(SQLException | ClassNotFoundException e )
+        {
+            System.out.println(e.getMessage());
+>>>>>>> parent of e19fdf8... gerer les bases de données
+        }
+        */
+        initDB();
+        
+        
     }
     
-    public static void initDBEmployee() {
+    public static void initDB()
+    {
         String url = "jdbc:mysql://localhost:3306/projet?useSSL=false";
+<<<<<<< HEAD
         String user = "root";
         String password = "";
 >>>>>>> 52e2d1a52a610653b346de6b20d74647712cc7f6
+=======
+            String user = "root";
+            String password = "" ;
+>>>>>>> parent of e19fdf8... gerer les bases de données
         Connection conn;
-
+        
         try {
+<<<<<<< HEAD
 <<<<<<< HEAD
             conn = (Connection) getDbConnection();
             Statement essai = conn.createStatement();
@@ -214,31 +235,21 @@ public class Menu {
                     + "  PRIMARY KEY (`login`)"
                     + ")");
 >>>>>>> 52e2d1a52a610653b346de6b20d74647712cc7f6
-        } catch (SQLException ex) {
-            Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    
-    public static void initDBCustomer() {
-        String url = "jdbc:mysql://localhost:3306/projet?useSSL=false";
-        String user = "root";
-        String password = "";
-        Connection conn;
-
-        try {
-            conn = (Connection) DriverManager.getConnection(url, user, password);
+=======
+            conn = (Connection) DriverManager.getConnection(url,user, password);
             Statement essai = conn.createStatement();
-            essai.execute("CREATE TABLE IF NOT EXISTS `CUSTOMER` ("
-                    + "  `title` varchar(100) NOT NULL,"
-                    + "  `type` varchar(100) NOT NULL,"
-                    + "  `releaseDate` date NOT NULL,"
-                    + "  `runningTime` time NOT NULL,"
-                    + "  `ticketPrice` double NOT NULL,"
-                    + "  PRIMARY KEY (`title`)"
-                    + ")");
+            essai.execute("CREATE TABLE IF NOT EXISTS `doublegangdripkodak` (" +
+"  `title` varchar(100) NOT NULL," +
+"  `type` varchar(100) NOT NULL," +
+"  `releaseDate` date NOT NULL," +
+"  `runningTime` time NOT NULL," +
+"  `ticketPrice` double NOT NULL," +
+"  PRIMARY KEY (`title`,`type`,`releaseDate`,`runningTime`,`ticketPrice`)" +
+")");
+>>>>>>> parent of e19fdf8... gerer les bases de données
         } catch (SQLException ex) {
             Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
 }
